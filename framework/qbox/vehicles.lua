d1a3e5410bc5d8426d2cecea0073e6151ff8d929
saikxyo <snowdicsdorolax@gmail.com>
2026-06-30 23:23:45 -0300

    Add files via upload

---
+++ framework/qbox/vehicles.lua
@@ -0,0 +1,722 @@
+return {
+    ['zr3806str'] = {
+        name = 'Annis ZR380 Custom',
+        brand = '6STR',
+        model = 'zr3806str',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zr3806str`,
+    },
+
+    ['schwarzer2'] = {
+        name = 'Benefactor Schwartzer Aggressor Custom',
+        brand = '6STR',
+        model = 'schwarzer2',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `schwarzer2`,
+    },
+
+    ['gauntlet6str'] = {
+        name = 'Bravado Gauntlet Custom',
+        brand = '6STR',
+        model = 'gauntlet6str',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `gauntlet6str`,
+    },
+
+    ['yosemite6str'] = {
+        name = 'Declasse Drift Yosemite',
+        brand = '6STR',
+        model = 'yosemite6str',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `yosemite6str`,
+    },
+
+    ['futo2'] = {
+        name = 'Karin Futo GT Hatch Custom',
+        brand = '6STR',
+        model = 'futo2',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `futo2`,
+    },
+
+    ['ruiner6str'] = {
+        name = 'Imponte Ruiner 450 Custom',
+        brand = '6STR',
+        model = 'ruiner6str',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `ruiner6str`,
+    },
+
+    ['tempesta2'] = {
+        name = 'Pegassi Tempesta Custom',
+        brand = '6STR',
+        model = 'tempesta2',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `tempesta2`,
+    },
+
+    ['sentinel6str2'] = {
+        name = 'Ubermacht Sentinel Classic Custom',
+        brand = '6STR',
+        model = 'sentinel6str2',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sentinel6str2`,
+    },
+
+    ['ellie6str'] = {
+        name = 'Vapid Drift Ellie Custom',
+        brand = '6STR',
+        model = 'ellie6str',
+        price = 200000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `ellie6str`,
+    },
+
+    ['elegyrh4'] = {
+        name = 'Elegy RH4',
+        brand = 'Annis',
+        model = 'elegyrh4',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `elegyrh4`,
+    },
+
+    ['elegyrh6'] = {
+        name = 'Elegy RH6',
+        brand = 'Annis',
+        model = 'elegyrh6',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `elegyrh6`,
+    },
+
+    ['elegyx'] = {
+        name = 'Elegy RH8-X',
+        brand = 'Annis',
+        model = 'elegyx',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `elegyx`,
+    },
+
+    ['zr250'] = {
+        name = 'ZR-250 "Savestra"',
+        brand = 'Annis',
+        model = 'zr250',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zr250`,
+    },
+
+    ['zr390'] = {
+        name = 'ZR390',
+        brand = 'Annis',
+        model = 'zr390',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zr390`,
+    },
+
+    ['dubsta4x4'] = {
+        name = 'Dubsta 4x4',
+        brand = 'Benefactor',
+        model = 'dubsta4x4',
+        price = 220000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `dubsta4x4`,
+    },
+
+    ['kriegerc'] = {
+        name = 'Krieger BPX-32B',
+        brand = 'Benefactor',
+        model = 'kriegerc',
+        price = 220000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `kriegerc`,
+    },
+
+    ['sancyb4'] = {
+        name = 'Sancy B4',
+        brand = 'Bordeaux',
+        model = 'sancyb4',
+        price = 100000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sancyb4`,
+    },
+
+    ['buffaloh'] = {
+        name = 'Buffalo Hellhound',
+        brand = 'Bravado',
+        model = 'buffaloh',
+        price = 190000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `buffaloh`,
+    },
+
+    ['gresleyh'] = {
+        name = 'Gresley Hellhound',
+        brand = 'Bravado',
+        model = 'gresleyh',
+        price = 190000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `gresleyh`,
+    },
+
+    ['verlierergt'] = {
+        name = 'Verlierer GT',
+        brand = 'Bravado',
+        model = 'verlierergt',
+        price = 190000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `verlierergt`,
+    },
+
+    ['vulture'] = {
+        name = 'Vulture',
+        brand = 'Bravado',
+        model = 'vulture',
+        price = 190000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `vulture`,
+    },
+
+    ['tampar'] = {
+        name = 'Restomod Tampa',
+        brand = 'Declasse',
+        model = 'tampar',
+        price = 160000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `tampar`,
+    },
+
+    ['supergts'] = {
+        name = 'SuperGTS',
+        brand = 'Dewbauchee',
+        model = 'supergts',
+        price = 450000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `supergts`,
+    },
+
+    ['millennial'] = {
+        name = 'Millennial Wagon',
+        brand = 'Dinka',
+        model = 'millennial',
+        price = 120000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `millennial`,
+    },
+
+    ['nexus'] = {
+        name = 'Nexus',
+        brand = 'Dinka',
+        model = 'nexus',
+        price = 120000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `nexus`,
+    },
+
+    ['serena'] = {
+        name = 'Serena',
+        brand = 'Dinka',
+        model = 'serena',
+        price = 120000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `serena`,
+    },
+
+    ['tachyon'] = {
+        name = 'Tachyon',
+        brand = 'Hijak',
+        model = 'tachyon',
+        price = 900000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `tachyon`,
+    },
+
+    ['coquette4c'] = {
+        name = 'Coquette D10 Widebody',
+        brand = 'Invetero',
+        model = 'coquette4c',
+        price = 380000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `coquette4c`,
+    },
+
+    ['s98'] = {
+        name = '98S/1',
+        brand = 'Karin',
+        model = 's98',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `s98`,
+    },
+
+    ['ariant'] = {
+        name = 'Ariant',
+        brand = 'Karin',
+        model = 'ariant',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `ariant`,
+    },
+
+    ['asteropers'] = {
+        name = 'Asterope RS',
+        brand = 'Karin',
+        model = 'asteropers',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `asteropers`,
+    },
+
+    ['boorc'] = {
+        name = 'Boor Widebody',
+        brand = 'Karin',
+        model = 'boorc',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `boorc`,
+    },
+
+    ['rebeld'] = {
+        name = 'Drift Rebel',
+        brand = 'Karin',
+        model = 'rebeld',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `rebeld`,
+    },
+
+    ['sigma3'] = {
+        name = 'Sigma-Three',
+        brand = 'Karin',
+        model = 'sigma3',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sigma3`,
+    },
+
+    ['sultan2c'] = {
+        name = 'Sultan Classic Custom',
+        brand = 'Karin',
+        model = 'sultan2c',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sultan2c`,
+    },
+
+    ['sultanrsv8'] = {
+        name = 'Sultan RS V8 TwinTurbo',
+        brand = 'Karin',
+        model = 'sultanrsv8',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sultanrsv8`,
+    },
+
+    ['sultanrsv82'] = {
+        name = 'Sultan RSV8 MK2',
+        brand = 'Karin',
+        model = 'sultanrsv82',
+        price = 150000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sultanrsv82`,
+    },
+
+    ['sunrise1'] = {
+        name = 'Sunrise R',
+        brand = 'Maibatsu',
+        model = 'sunrise1',
+        price = 140000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sunrise1`,
+    },
+
+    ['niner'] = {
+        name = '9R',
+        brand = 'Obey',
+        model = 'niner',
+        price = 300000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `niner`,
+    },
+
+    ['argento'] = {
+        name = 'Argento',
+        brand = 'Obey',
+        model = 'argento',
+        price = 300000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `argento`,
+    },
+
+    ['mantis'] = {
+        name = 'Mantis',
+        brand = 'Ocelot',
+        model = 'mantis',
+        price = 500000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `mantis`,
+    },
+
+    ['bati901'] = {
+        name = 'Bati 901',
+        brand = 'Pegassi',
+        model = 'bati901',
+        price = 700000,
+        category = 'importados',
+        type = 'bike',
+        hash = `bati901`,
+    },
+
+    ['infernussr'] = {
+        name = 'Infernus S Roadster',
+        brand = 'Pegassi',
+        model = 'infernussr',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `infernussr`,
+    },
+
+    ['matador'] = {
+        name = 'Matador',
+        brand = 'Pegassi',
+        model = 'matador',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `matador`,
+    },
+
+    ['monroei'] = {
+        name = 'Monroe (Improved)',
+        brand = 'Pegassi',
+        model = 'monroei',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `monroei`,
+    },
+
+    ['monroec'] = {
+        name = 'Monroe Custom',
+        brand = 'Pegassi',
+        model = 'monroec',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `monroec`,
+    },
+
+    ['monroer'] = {
+        name = 'Monroe Roadster',
+        brand = 'Pegassi',
+        model = 'monroer',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `monroer`,
+    },
+
+    ['monroeiw'] = {
+        name = 'Monroe Widebody',
+        brand = 'Pegassi',
+        model = 'monroeiw',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `monroeiw`,
+    },
+
+    ['monroerw'] = {
+        name = 'Monroe Roadster Widebody',
+        brand = 'Pegassi',
+        model = 'monroerw',
+        price = 700000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `monroerw`,
+    },
+
+    ['p711'] = {
+        name = '711',
+        brand = 'Pfister',
+        model = 'p711',
+        price = 650000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `p711`,
+    },
+
+    ['callista'] = {
+        name = 'Comet Callista',
+        brand = 'Pfister',
+        model = 'callista',
+        price = 650000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `callista`,
+    },
+
+    ['cometr'] = {
+        name = 'Comet R',
+        brand = 'Pfister',
+        model = 'cometr',
+        price = 650000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `cometr`,
+    },
+
+    ['mf1'] = {
+        name = 'MF1 Spyder',
+        brand = 'Progen',
+        model = 'mf1',
+        price = 950000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `mf1`,
+    },
+
+    ['mf1c'] = {
+        name = 'MF1 Spyder (Custom)',
+        brand = 'Progen',
+        model = 'mf1c',
+        price = 950000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `mf1c`,
+    },
+
+    ['t20gtr'] = {
+        name = 'T20 GTR',
+        brand = 'Progen',
+        model = 't20gtr',
+        price = 950000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `t20gtr`,
+    },
+
+    ['castella'] = {
+        name = 'Castella',
+        brand = 'Shitzu',
+        model = 'castella',
+        price = 110000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `castella`,
+    },
+
+    ['castellajp'] = {
+        name = 'Castella',
+        brand = 'Shitzu',
+        model = 'castellajp',
+        price = 110000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `castellajp`,
+    },
+
+    ['adders'] = {
+        name = 'Adder Sport',
+        brand = 'Truffade',
+        model = 'adders',
+        price = 1500000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `adders`,
+    },
+
+    ['sentinelmk4'] = {
+        name = 'Sentinel Mk.4',
+        brand = 'Ubermacht',
+        model = 'sentinelmk4',
+        price = 260000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sentinelmk4`,
+    },
+
+    ['sentinelmk4gtr'] = {
+        name = 'Sentinel Mk.4 GTR',
+        brand = 'Ubermacht',
+        model = 'sentinelmk4gtr',
+        price = 260000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sentinelmk4gtr`,
+    },
+
+    ['sentineldm'] = {
+        name = 'Sentinel SG4 Drift Missile',
+        brand = 'Ubermacht',
+        model = 'sentineldm',
+        price = 260000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sentineldm`,
+    },
+
+    ['vorstand'] = {
+        name = 'Vorstand',
+        brand = 'Ubermacht',
+        model = 'vorstand',
+        price = 260000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `vorstand`,
+    },
+
+    ['contenderc'] = {
+        name = 'Contender Custom',
+        brand = 'Vapid',
+        model = 'contenderc',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `contenderc`,
+    },
+
+    ['domgtcoupe'] = {
+        name = 'Dominator GT Coupe',
+        brand = 'Vapid',
+        model = 'domgtcoupe',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `domgtcoupe`,
+    },
+
+    ['sabot'] = {
+        name = 'Sabot',
+        brand = 'Vapid',
+        model = 'sabot',
+        price = 180000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `sabot`,
+    },
+
+    ['hachura'] = {
+        name = 'Hachura R',
+        brand = 'Vulcar',
+        model = 'hachura',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `hachura`,
+    },
+
+    ['hachurac'] = {
+        name = 'Hachura R Custom',
+        brand = 'Vulcar',
+        model = 'hachurac',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `hachurac`,
+    },
+
+    ['r255'] = {
+        name = 'Ingot R255',
+        brand = 'Vulcar',
+        model = 'r255',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `r255`,
+    },
+
+    ['zodiac'] = {
+        name = 'Zodiac',
+        brand = 'Vulcar',
+        model = 'zodiac',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zodiac`,
+    },
+
+    ['zodiacc'] = {
+        name = 'Zodiac',
+        brand = 'Vulcar',
+        model = 'zodiacc',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zodiacc`,
+    },
+
+    ['zodiacr'] = {
+        name = 'Zodiac',
+        brand = 'Vulcar',
+        model = 'zodiacr',
+        price = 130000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `zodiacr`,
+    },
+
+    ['stratumc'] = {
+        name = 'Stratum',
+        brand = 'Zircoflow',
+        model = 'stratumc',
+        price = 140000,
+        category = 'importados',
+        type = 'automobile',
+        hash = `stratumc`,
+    },
+
+}
